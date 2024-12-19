3c3d861ad8eb67a4303816cebeedca175ab27889
Charlotte <cmjwfeltwell@gmail.com>
2024-12-18 20:49:08 -0600

    Create cpu.h

---
+++ include/cpu.h
@@ -0,0 +1,38 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include <cstdint>
+
+class CPU {
+public:
+	/* registers */
+
+	// B and C, general purpose
+	uint8_t B;
+	uint8_t C;
+
+	// D and E, general purpose
+	uint8_t D;
+	uint8_t E;
+
+	// H and L, general purpose
+	uint8_t H;
+	uint8_t L;
+
+	// W and Z, temp registers
+	uint8_t W;
+	uint8_t Z;
+
+	// accumulator
+	uint8_t A;
+
+	// program counter
+	uint16_t PC;
+
+	// stack pointer
+	uint16_t SP;
+
+
+};
+
+#endif CPU_H
